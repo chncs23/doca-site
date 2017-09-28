27e1fb0eeb4f115516038f1bf9dadcf5e38865e2
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-09-28 11:22:52 +0700

    ปรับปรุงเมนูให้อ้างไฟล์ใน server แทน url

--- scriptmenu.php
+++ scriptmenu.php
@@ -1,39 +1,39 @@
 <nav id="menu-wrap">
 <div id="menu-trigger">Menu</div>
 <ul id="menu">
-    <li><a href="http://doca.rta.mi.th/home.php">หน้าหลัก</a></li>
+    <li><a href="home.php">หน้าหลัก</a></li>
     <li><a href="#">เกี่ยวกับหน่วย</a>
         <ul>
-            <li><a href="http://doca.rta.mi.th/doca_history.php">ประวัติหน่วย</a></li>
-            <li><a href="http://doca.rta.mi.th/doca_commander.php">ทำเนียบผู้บังคับบัญชา</a></li>
-            <li><a href="http://doca.rta.mi.th/doca_formercommander.php">อดีตผู้บังคับบัญชา</a></li>
-        </ul>  
+            <li><a href="doca_history.php">ประวัติหน่วย</a></li>
+            <li><a href="doca_commander.php">ทำเนียบผู้บังคับบัญชา</a></li>
+            <li><a href="doca_formercommander.php">อดีตผู้บังคับบัญชา</a></li>
+        </ul>
     </li>
     <li><a href="#">โครงสร้างหน่วย</a>
     	<ul>
                <li><a href="#">สำนักจิตวิทยา</a>
                     <ul>
-                        <li><a href="http://doca.rta.mi.th/dep_psycho.php">กองปฏิบัติการจิตวิทยา</a></li>
-                        <li><a href="http://doca.rta.mi.th/dep_masses.php">กองกิจการมวลชน</a></li>
-                        <li><a href="http://doca.rta.mi.th/dep_pr.php">กองประชาสัมพันธ์</a></li>
+                        <li><a href="dep_psycho.php">กองปฏิบัติการจิตวิทยา</a></li>
+                        <li><a href="dep_masses.php">กองกิจการมวลชน</a></li>
+                        <li><a href="dep_pr.php">กองประชาสัมพันธ์</a></li>
                     </ul>
               	</li>
                 <li><a href="#">สำนักกิจการพลเรือน</a>
                     <ul>
-                        <li><a href="http://doca.rta.mi.th/dep_civil_affair.php">กองกิจการพลเรือน</a></li>
-                        <li><a href="http://doca.rta.mi.th/dep_development.php">กองกิจการพัฒนา</a></li>
-                        <li><a href="http://doca.rta.mi.th/dep_disaster.php">กองบรรเทาสาธารณะภัย</a></li>
+                        <li><a href="dep_civil_affair.php">กองกิจการพลเรือน</a></li>
+                        <li><a href="dep_development.php">กองกิจการพัฒนา</a></li>
+                        <li><a href="dep_disaster.php">กองบรรเทาสาธารณะภัย</a></li>
 
                     </ul>
               	</li>
-				<li><a href="http://doca.rta.mi.th/dep_administration.php">กองธุรการ</a></li>
-                <li><a href="http://doca.rta.mi.th/dep_planning.php">กองนโยบายและแผน</a></li>
-                <li><a href="http://doca.rta.mi.th/dep_it.php">กองสารสนเทศ</a></li>
-                <li><a href="http://doca.rta.mi.th/dep_budget.php">กองโครงการและงบประมาณ</a></li>
+				<li><a href="dep_administration.php">กองธุรการ</a></li>
+                <li><a href="dep_planning.php">กองนโยบายและแผน</a></li>
+                <li><a href="dep_it.php">กองสารสนเทศ</a></li>
+                <li><a href="dep_budget.php">กองโครงการและงบประมาณ</a></li>
                 <li><a href="http://e-civil.rta.mi.th/">โรงเรียนกิจการพลเรือน</a></li>
 		</ul>
     </li>
-    <li><a href="http://doca.rta.mi.th/doca_mission.php">ภารกิจหน่วย</a></li>
-    <li><a href="http://doca.rta.mi.th/doca_king.php">เทิดพระเกียรติ</a></li>
+    <li><a href="doca_mission.php">ภารกิจหน่วย</a></li>
+    <li><a href="doca_king.php">เทิดพระเกียรติ</a></li>
 </ul>
-</nav>    
+</nav>
