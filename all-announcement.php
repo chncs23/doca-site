c86abfea83cbb4b33bb67a986ec7b73efc522cbb
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-10-04 10:50:10 +0700

    remove unused script

--- all-announcement.php
+++ all-announcement.php
@@ -1,5 +1,3 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="bootstrap/js/bootstrap.js"></script>
 <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/all-news-content.css" />
 
--- doca-webmanagement.php
+++ doca-webmanagement.php
@@ -24,14 +24,6 @@
 <body topmargin="">
 <div id="fb-root"></div>
 
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
 <table width="1120" border="0" align="center" cellpadding="0" cellspacing="0">
   <tr valign="top">
     <td colspan="4">
