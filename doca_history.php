63fceec127ad662671f9280a058da675f6e1aa33
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-10-09 14:56:52 +0700

    ปรับปรุงหน้าประวัติ กร.ทบ.

--- doca_history.php
+++ doca_history.php
@@ -1,162 +1,88 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link rel="stylesheet" type="text/css" href="css/layout.css" />
-<link rel="stylesheet" type="text/css" href="css/style2.css" />
-<link rel="stylesheet" type="text/css" href="css/dropmenu.css" />
+	<link rel="stylesheet" type="text/css" href="css/layout.css" />
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="bootstrap/js/bootstrap.js"></script>
-<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css"> 
-<!-- Script Silde Image-->
-<script language="javascript" type="text/javascript" src="js/jquery.js"></script>
-<script language="javascript" type="text/javascript" src="js/jquery.easing.js"></script>
-<script language="javascript" type="text/javascript" src="js/script.js"></script>
-<style>
-	ul.lof-main-wapper li {
-		position:relative;
-	}
-	.style18 {font-size: 16pt; font-weight: ;  font-family: "TH SarabunPSK", sans-serif; }
-	.style22 {font-size: 24pt; font-weight: bold:; color: #F00; font-family: "TH SarabunPSK", sans-serif; }
-body {
-	background-image: url(..images/BG7.jpg);
-	background-repeat: repeat;
-	margin-left: 0px;
-	margin-top: 0px;
-	margin-right: 0px;
-	margin-bottom: 0px;
-	line-height:normal;
-}
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 
 
-</style>
-
-
-  <!-- ------------------------------------------------------test Menu-->
-<style type="text/css">
-/* เมนูปกติ */
-.css_menu{
-    position:relative;
-    display:block;
-    float:left;
-    width:100px;
-    height:25px;
-    background-color:#333;
-    color: #FFF;
-    text-align:center;
-    text-decoration:none;
-    line-height:25px;
-    margin-right:2px;
-}
-/*  เมนูเมื่อเมาส์ over*/
-.css_menu:hover{
-    position:relative;
-    display:block;
-    float:left;
-    width:100px;
-    height:25px;
-    background-color:#CCC;
-    color:#000;
-}
-/*  เมนูเมื่อ active*/
-.css_menu_active{
-    position:relative;
-    display:block;
-    float:left;
-    width:100px;
-    height:25px;
-    background-color:#CCC;
-    color:#000;
-    text-align:center;
-    text-decoration:underline;
-    line-height:25px;
-    margin-right:2px;
-}
-
-a:link {
-	text-decoration: none;
-}
-a:visited {
-	text-decoration: none;
-}
-a:hover {
-	text-decoration: none;
-}
-a:active {
-	text-decoration: none;
-}
-</style>
-
-<title>..ประวัติกรมกิจการพลเรือนทหารบก..</title>
+	<title>ประวัติกรมกิจการพลเรือนทหารบก</title>
 </head>
-<body topmargin="">
-<div id="fb-root"></div>
 
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+<style media="screen">
+.content {
+	line-height: 30px;
+}
 
-<table width="1120" border="0" align="center" cellpadding="0" cellspacing="0">
-  <tr valign="top">
-    <td colspan="4">
-   <?php
+</style>
+
+<body>
+	<?php
 	require_once("scriptmenu.php");
 	?>
- <!-- End Menu ----------------------------------->
-    </td>
-</table>
-</table>
-<table width="1120" border="0" align="center" cellpadding="0" cellspacing="0">
-<tr>
-    <td width="1120" height="120" valign="top" align="center"><img src="images/doca_history_header.png" width="1120" height="120" /></td>
-  </tr>
-</table>
-<table width="1120" border="0" align="center" cellpadding="0" cellspacing="0" background="images/baseBG.png">
-<tr>
-  <td width="135" rowspan="11" align="center"><p>&nbsp;</p>
-    <p class="style18">&nbsp;</p>
-    </td>
-  <td colspan="2" align="left" valign="middle"><p class="style18">จากสถานการณ์ ของประเทศในช่วงสงครามเย็น ซึ่งเกิดจากการเผชิญหน้าทางอุดมการณ์ระหว่างค่ายทุนนิยม กับค่ายสังคมนิยม ทำให้ประเทศชาติต้องตกอยู่ภายใต้ภัยคุกคามจากลัทธิคอมมิวนิสต์ งานการปลูกฝังอุดมการณ์ ทางการเมือง การช่วยเหลือประชาชนตลอดจนงานปฏิบัติการจิตวิทยา จึงเป็นงานสำคัญที่รัฐบาล โดยฝ่ายทหารได้ถูกนำมาใช้ ในการต่อสู้เพื่อเอาชนะคอมมิวนิสต์ งานต่างๆดังกล่าว ถือเป็นจุดเริ่มต้นของงานด้านกิจการพลเรือนและได้มีการขยายขอบเขต เพิ่มเติม โดยเฉพาะ ในช่วงการใช้นโยบายการเมืองนำการทหารตามคำสั่งสำนักนายกรัฐมนตรี ที่ 66/23 จนถึงปัจจุบัน</p></td>
-  <td width="135" rowspan="11" align="left" valign="top">
-  </td>
-</tr>
-<tr>
-  <td width="195" align="center" valign="top">
-  <img src="images/commander-prem.jpg" width="150" height="200" /></td>
-  <td width="862" align="center" valign="top"><p class="style18">หน่วยงานของกองทัพบก ที่จัดตั้งขึ้นมารับผิดชอบงานกิจการพลเรือน เริ่มดำเนินการอย่างจริงจังเมื่อ ปี พ.ศ. 2522 โดยได้กำหนดให้มีการจัดตั้งหน่วย ในระดับรอง คือ ระดับกองทัพภาค มีการจัดตั้งกองกิจการพลเรือนและในระดับกองพล ได้จัดตั้งแผนกกิจการพลเรือน ทำหน้าที่ ฝ่ายอำนวยการในสายงานกิจการพลเรือนโดยเฉพาะ และในเวลาต่อมา พล.อ.เปรม ติณสูลานนท์ ผบ.ทบ. ในขณะนั้น ได้ดำริเห็นว่า ในระดับกองทัพบกงานกิจการพลเรือน ยังไม่มีกรมฝ่ายอำนวยการไดเป็นผู้รับผิดชอบอย่างแท้จริง ทำให้ประสบกับปัญหา การประสานงาน การกำหนดนโยบายและการควบคุมกำกับดูแล จึงดำริให้จัดตั้ง กรมกิจการพลเรือนทหารบก ตามคำสั่งกองทัพบก (เฉพาะ) ลับ ที่ 6/25 ลง 13 มกราคม 2525</p></td>
-</tr>
 
-<tr>
-  <td colspan="2" align="center" valign="middle"><p class="style18">กรมกิจการพลเรือนใช้นามหน่วย โดยย่อ ว่า &quot;กร.ทบ.&quot; และใช้เครื่องหมายสังกัด &quot;กร&quot; มีตราประจำหน่วยเป็นรูปของ &quot;ท้าวศักรินทร์&quot; บรรจุในวงกลมภายใน รอบวงกลมด้านนอก มีตัวอักษรว่า &quot;กรมกิจการพลเรือนทหารบก&quot; และด้านล่างมีตัวอักษรภาษาอังกฤษว่า &quot;DIRECTORATE OF CIVIL AFFAIRS&quot;</p>
-    <p class="style18"><img src="images/doca-logo.gif" alt="" width="175" height="175" /></p>
-    <p class="style18">&quot;ท้าวศักรินทร์&quot; มีประวัติความสัมพันธ์ใกล้ชิดกับมนุษย์ เป็นอย่างมาก ตามหลักฐานทางศาสนาและวรรณคดี ปรากฏหน้าที่ส่วนใหญ่ คอยช่วยเหลือเกื้อกูลผู้ประสบความทุกข์ร้อน เปรียบได้กับนักสังคมสงเคราะหฺ์ในปัจจุบัน ซึ่งสอดคล้องกับภารกิจของกรมกิจการพลเรือนทหารบก</p></td>
-  </tr>
-<tr>
-  <td colspan="2" align="left" valign="top"></td>
-</tr>
-<tr>
-  <td align="center" valign="top">
-  	<img src="images/doca_formercommander/1_doca_formercommander_Vichit.png" width="150" height="200" />
-  </td>
-  <td align="center" valign="top"><p class="style18">กรมกิจการพลเรือนทหารบกจึงได้ยึดถือวันที่ 13 มกราคม 2525 ซึ่งเป็นวันที่คำสั่งกองทัพบกให้จัดตั้งกรมกิจการพลเรือนทหารบก เป็นวันสถาปนาหน่วย โดยเมื่อแรกจัดตั้ง มีที่ตั้งปกติอยู่ชั่วคราวในบริเวณ ศาลาว่าการกระทรวงกลาโหม </p>
-    <p class="style18">โดยมี พลตรี วิชิต บุณยะวัฒน์ เป็นเจ้ากรมกิจการพลเรือนทหารบก ท่านแรก
-    และต่อมาได้ย้ายเข้าที่ตั้งปกติถาวร ณ กองบัญชาการกองทัพบก ถนนราชดำเนินนอก เขตบางขุนพรม กรุงเทพมหานคร</p></td>
-</tr>
-<tr>
-  <td colspan="2" align="center" valign="top" ><p class="style18"> ที่ผ่านมามีผู้ดำรงตำแหน่งเจ้ากรมกิจการพลเรือนทหารบกมาแล้ว 13 ท่าน โดยปัจจุบันมี
-  <a href="http://doca.rta.mi.th/doca_commander.php" target="_blank">พล.ท. ธเนศ กาลพฤกษ์ </a>เป็นเจ้ากรมกิจการพลเรือนทหารบก</p>
-    <p class="style18"><img src="images/seperater2.png" alt="" width="400" height="20" /></p>
-    </td>
-  </tr>
-</table>
-<!-- <p class="style18"><img src="images/doca_commander/commander1_Teerawat.png" alt="" width="150" height="200" /></p> --!>
+	<div class="container nopadding-horizontal">
+		<img src="images/DocaBanner.png" width=100%/>
+	</div>
 
+	<div class="container">
+		<div class="page-header">
+			<h1>ประวัติกรมกิจการพลเรือนทหารบก</h1>
+		</div>
+		<div class='content-padding'>
+			<div class="row">
+				<div class="col-md-12 content ">
+					<p>จากสถานการณ์ของประเทศในช่วงสงครามเย็น ซึ่งเกิดจากการเผชิญหน้าทางอุดมการณ์ระหว่างค่ายทุนนิยม กับค่ายสังคมนิยม ทำให้ประเทศชาติต้องตกอยู่ภายใต้ภัยคุกคามจากลัทธิคอมมิวนิสต์ งานการปลูกฝังอุดมการณ์ ทางการเมือง การช่วยเหลือประชาชนตลอดจนงานปฏิบัติการจิตวิทยา จึงเป็นงานสำคัญที่รัฐบาล โดยฝ่ายทหารได้ถูกนำมาใช้ ในการต่อสู้เพื่อเอาชนะคอมมิวนิสต์ งานต่างๆดังกล่าว ถือเป็นจุดเริ่มต้นของงานด้านกิจการพลเรือนและได้มีการขยายขอบเขต เพิ่มเติม โดยเฉพาะ ในช่วงการใช้นโยบายการเมืองนำการทหารตามคำสั่งสำนักนายกรัฐมนตรี ที่ 66/23 จนถึงปัจจุบัน</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-2" align="center">
+					<img src="images/commander-prem.jpg" width="150" height="200" />
+				</div>
+				<div class="col-md-10 content">
+					<p>หน่วยงานของกองทัพบก ที่จัดตั้งขึ้นมารับผิดชอบงานกิจการพลเรือน เริ่มดำเนินการอย่างจริงจังเมื่อ ปี พ.ศ. 2522 โดยได้กำหนดให้มีการจัดตั้งหน่วย ในระดับรอง คือ ระดับกองทัพภาค มีการจัดตั้งกองกิจการพลเรือนและในระดับกองพล ได้จัดตั้งแผนกกิจการพลเรือน ทำหน้าที่ ฝ่ายอำนวยการในสายงานกิจการพลเรือนโดยเฉพาะ และในเวลาต่อมา พล.อ.เปรม ติณสูลานนท์ ผบ.ทบ. ในขณะนั้น ได้ดำริเห็นว่า ในระดับกองทัพบกงานกิจการพลเรือน ยังไม่มีกรมฝ่ายอำนวยการไดเป็นผู้รับผิดชอบอย่างแท้จริง ทำให้ประสบกับปัญหา การประสานงาน การกำหนดนโยบายและการควบคุมกำกับดูแล จึงดำริให้จัดตั้ง กรมกิจการพลเรือนทหารบก ตามคำสั่งกองทัพบก (เฉพาะ) ลับ ที่ 6/25 ลง 13 มกราคม 2525</p>
+				</div>
+			</div>
+			<p>&nbsp;</p>
+			<div class="row">
+				<div class="col-md-12 content">
+					<p>กรมกิจการพลเรือนใช้นามหน่วยโดยย่อว่า <b>&quot;กร.ทบ.&quot;</b> และใช้เครื่องหมายสังกัด <b>&quot;กร&quot;</b> มีตราประจำหน่วยเป็นรูปของ <b>&quot;ท้าวศักรินทร์&quot;</b> บรรจุในวงกลมภายใน รอบวงกลมด้านนอก มีตัวอักษรว่า <b>&quot;กรมกิจการพลเรือนทหารบก&quot;</b> และด้านล่างมีตัวอักษรภาษาอังกฤษว่า <b>&quot;DIRECTORATE OF CIVIL AFFAIRS&quot;</b></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12" align="center">
+					<img src="images/doca-logo.gif" alt="" width="175" height="175"/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 content">
+					<p><b>&quot;ท้าวศักรินทร์&quot;</b> มีประวัติความสัมพันธ์ใกล้ชิดกับมนุษย์เป็นอย่างมาก ตามหลักฐานทางศาสนาและวรรณคดี ปรากฏหน้าที่ส่วนใหญ่ คอยช่วยเหลือเกื้อกูลผู้ประสบความทุกข์ร้อน เปรียบได้กับนักสังคมสงเคราะห์ในปัจจุบัน ซึ่งสอดคล้องกับภารกิจของกรมกิจการพลเรือนทหารบก</p>
+				</div>
+			</div>
+			<p>&nbsp;</p>
+			<div class="row">
+				<div class="col-md-2" align="center">
+					<img src="images/doca_formercommander/1_doca_formercommander_Vichit.png" width="150" height="200" />
+				</div>
+				<div class="col-md-10 content">
+					<p>กรมกิจการพลเรือนทหารบกจึงได้ยึดถือวันที่ 13 มกราคม 2525 ซึ่งเป็นวันที่คำสั่งกองทัพบกให้จัดตั้งกรมกิจการพลเรือนทหารบก เป็นวันสถาปนาหน่วย โดยเมื่อแรกจัดตั้ง มีที่ตั้งปกติอยู่ชั่วคราวในบริเวณ ศาลาว่าการกระทรวงกลาโหม โดยมี พลตรี วิชิต บุณยะวัฒน์ เป็นเจ้ากรมกิจการพลเรือนทหารบก ท่านแรก และต่อมาได้ย้ายเข้าที่ตั้งปกติถาวร ณ กองบัญชาการกองทัพบก ถนนราชดำเนินนอก เขตบางขุนพรหม กรุงเทพมหานคร	</p>
+					<p>ที่ผ่านมามีผู้ดำรงตำแหน่งเจ้ากรมกิจการพลเรือนทหารบกมาแล้ว 13 ท่าน โดยปัจจุบันมี <a href="http://doca.rta.mi.th/doca_commander.php" target="_blank">พล.ท. ธเนศ กาลพฤกษ์</a> เป็นเจ้ากรมกิจการพลเรือนทหารบก</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 content">
+					<p></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="footer">
+		<?php include("footer.html") ?>
+	</footer>
 </body>
 </html>
