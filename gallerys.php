8d47eff2218358524d46de09b400564fca3772da
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-10-09 14:50:38 +0700

    ปรับปรุงหน้ารายละเอียดข่าว (เสร็จแล้ว)

--- gallerys.php
+++ gallerys.php
@@ -10,7 +10,7 @@ $Rid = $_GET['Rid'];
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="css/layout.css" />
 	<link rel="stylesheet" type="text/css" href="css/style2.css" />
-  <link href="https://fonts.googleapis.com/css?family=Taviraj:300,500" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Taviraj:300,500" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link type="text/css" rel="stylesheet" href="lightslider/css/lightslider.css" />
 	<script src="lightslider/js/lightslider.js"></script>
@@ -41,9 +41,9 @@ $(document).ready(function() {
 </script>
 
 <style >
-	p {
-		font-size: 18px;
-	}
+.content {
+	font-size: 18px;
+}
 </style>
 
 <body>
@@ -67,36 +67,40 @@ $(document).ready(function() {
 
 			<div class="page-header">
 				<h3><?php echo $objResult["act_topic"]; ?></h3>
-				<p><b>วันที่เผยแพร่ :</b> <?php echo $objResult["act_date"]; ?></p>
+				<p class="content"><b>วันที่เผยแพร่ :</b> <?php echo $objResult["act_date"]; ?></p>
 			</div>
 
-			<p><?php echo $objResult["act_details"]; ?></p>
+			<p class="content"><?php echo $objResult["act_details"]; ?></p>
 
 		<?php } ?>
 
 
 		<div class="well">
 
-				<div class="clearfix center" style="max-width:80%;">
-					<ul id="image-gallery" class="gallery list-unstyled cS-hidden">
-						<?php  // Script แสดง Gallery ภาพ
-						require_once("callconnectionimproved.php");
+			<div class="clearfix center" style="max-width:80%;">
+				<ul id="image-gallery" class="gallery list-unstyled cS-hidden">
+					<?php  // Script แสดง Gallery ภาพ
+					require_once("callconnectionimproved.php");
 
-						$strSQL = "SELECT * FROM image WHERE act_id like '%".$Rid."%'";
-						$objQuery = mysqli_query($conn, $strSQL) or die ("Error Query [".$strSQL."]");
-						$total=mysqli_num_rows($objQuery);
+					$strSQL = "SELECT * FROM image WHERE act_id like '%".$Rid."%'";
+					$objQuery = mysqli_query($conn, $strSQL) or die ("Error Query [".$strSQL."]");
+					$total=mysqli_num_rows($objQuery);
 
-						if($total>0) {
-							while ($objResult=mysqli_fetch_array($objQuery)) {?>
-								<li data-thumb='<?php echo $objResult["image_path"]; ?>'>
-									<img src='<?php echo $objResult["image_path"];?>' width="100%" height="50%">
-								</li>
-							<?php  }
-						}?>
-					</ul>
-				</div>
+					if($total>0) {
+						while ($objResult=mysqli_fetch_array($objQuery)) {?>
+							<li data-thumb='<?php echo $objResult["image_path"]; ?>'>
+								<img src='<?php echo $objResult["image_path"];?>' width="100%" height="50%">
+							</li>
+						<?php  }
+					}?>
+				</ul>
+			</div>
 
-</div>
+		</div>
 	</div>
+
+	<footer class="footer">
+		<?php include("footer.html") ?>
+	</footer>
 </body>
 </html>
