e229bbdf913da4352eca8a9a22f9df33a499cb7c
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-11-15 14:02:44 +0700

    เพิ่มเมนูจัดการ จัดซื้อจัดจ้าง

--- scriptmenueditor.php
+++ scriptmenueditor.php
@@ -10,6 +10,7 @@
         <a class="css_menu<?=active_menu("/manage-fft.php?menu=1")?>" href="manage-fft.php" ><span class="style18">ข้อความด่วน</span></a>
         <a class="css_menu<?=active_menu("/manage-activity.php?menu=2")?>" href="manage-activity.php"><span class="style18">ข่าวกิจกรรม</span></a>
         <a class="css_menu<?=active_menu("/manage-activity.php?menu=3")?>" href="manage-announcement.php"><span class="style18">ข่าวประชาสัมพันธ์</span></a>
+				<a class="css_menu<?=active_menu("/manage-activity.php?menu=3")?>" href="manage-procurement.php"><span class="style18">จัดซื้อจัดจ้าง</span></a>
 				<a class="css_menu<?=active_menu("/manage-journal.php?menu=5")?>" href="manage-journal.php"><span class="style18">DocaJournal</span></a>
         <a class="css_menu<?=active_menu("/manage-channel.php?menu=6")?>" href="manage-channel.php"><span class="style18">DocaChannel</span></a>
         <a class="css_menu<?=active_menu("/manage-custom-menu.php?menu=7")?>" href="manage-custom-menu.php"><span class="style18">เมนูซ้าย</span></a>
