d743583ec70e31a688ab92fd641a70867820a930
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-11-15 14:00:33 +0700

    ปรับหน้าแรก เป็นหน้า ร10

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>กรมกิจการพลเรือนทหารบก</title>
-<style>
+<style>	
 body {
-	background-image: url(images/bg-yellow.jpg);
+	background-image: url(../images/bg-yellow.jpg);
 	background-repeat: repeat;
 	margin-left: 0px;
 	margin-top: 0px;
@@ -18,14 +18,14 @@ body {
 <body>
 <table  border="0" align="center" cellspacing="10">
   <tr valign="middle">
-    	<td width="275" height="350" align="center" ><img src="images/Rama10.png" /></td>
+    	<td width="275" height="350" align="center" ><img src="/images/Rama10.png" /></td>
   </tr>
    <tr valign="middle">
-     <td height="100" align="center" ><img src="images/bless-rama10.png" /></td>
+     <td height="100" align="center" ><img src="/images/bless-rama10.png" /></td>
   </tr>
   <tr valign="middle">
-        <td height="50" align="center" > <img src="images/seperater2.png" alt="" width="150" height="20" /><br />
-        <a href="home.php"><img src="images/enterweb-nologo.png" alt="" /></a>
+        <td height="50" align="center" > <img src="/images/seperater2.png" alt="" width="150" height="20" /><br />
+        <a href="http://doca.rta.mi.th/home.php"><img src="/images/enterweb-nologo.png" alt="" /></a>
         </td>
   </tr>
 </table>
