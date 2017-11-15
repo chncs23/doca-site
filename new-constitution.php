4bd6451bcb784b34843800dc2b1fced950f3426b
Choatravee Nhanaen <ch.n.17699@gmail.com>
2017-11-15 16:37:49 +0700

    แก้บัคเพิ่มเติม หลังจากที่ปรับปรุงเว็บใหม่

---
+++ new-constitution.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="stylesheet" type="text/css" href="css/layout.css" />
+<link rel="stylesheet" type="text/css" href="css/style2.css" />
+<link rel="stylesheet" type="text/css" href="css/dropmenu.css" />
+      
+</script>  
+<!-- Script Silde Image-->   
+<script language="javascript" type="text/javascript" src="js/jquery.js"></script>
+<script language="javascript" type="text/javascript" src="js/jquery.easing.js"></script>
+<script language="javascript" type="text/javascript" src="js/script.js"></script>
+<style>	
+	ul.lof-main-wapper li {
+		position:relative;	
+	}
+.style18 {font-size: 16pt; font-weight: ; font-family: "TH SarabunPSK", sans-serif; }
+.style22 {font-size: 24pt; font-weight: bold; font-family: "TH SarabunPSK", sans-serif; }
+body {
+	background-image: url(../images/BG7.jpg);
+	background-repeat: repeat;
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+	
+
+</style>
+
+
+  <!-- ------------------------------------------------------test Menu--> 
+<style type="text/css">  
+/* เมนูปกติ */  
+.css_menu{  
+    position:relative;  
+    display:block;  
+    float:left;  
+    width:100px;  
+    height:25px;  
+    background-color:#333;  
+    color: #FFF;   
+    text-align:center;  
+    text-decoration:none;  
+    line-height:25px;  
+    margin-right:2px;  
+}  
+/*  เมนูเมื่อเมาส์ over*/  
+.css_menu:hover{  
+    position:relative;  
+    display:block;  
+    float:left;  
+    width:100px;  
+    height:25px;  
+    background-color:#CCC;  
+    color:#000;   
+}  
+/*  เมนูเมื่อ active*/  
+.css_menu_active{  
+    position:relative;  
+    display:block;  
+    float:left;  
+    width:100px;  
+    height:25px;  
+    background-color:#CCC;  
+    color:#000;   
+    text-align:center;  
+    text-decoration:underline;  
+    line-height:25px;  
+    margin-right:2px;  
+} 
+</style>
+
+<title>..รายการแกะกล่อง รัฐธรรมนูญใหม่..</title>
+</head>
+<body topmargin="">
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<table width="1120" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr valign="top">
+    <td colspan="4">
+   <?php 
+	require_once("scriptmenu.php");
+	?>   
+    </td>
+</table>
+
+</table>
+<table width="1120" border="0" align="center" cellpadding="10" cellspacing="10">
+<tr>
+  <td height="150" align="center" background="images/baseBG.png"><p>&nbsp;</p>
+    <p><span class="style22">รายการแกะกล่อง</strong> รัฐธรรมนูญใหม่</span></p>
+    <p><span class="style22">ออกอากาศหลังเคารพธงชาติ 08:00 และ 18:00 ทางโทรทัศน์รวมการเฉพาะกิจแห่งประเทศไทย</span></p>
+    <p><span class="style22">
+    <img src="images/new-constitution-poster-crop.jpg" alt="" width="1120" height="" border="0"/>
+    <br>
+    </span></p>    
+    
+  </td>
+  </tr>
+<tr>
+  <td align="center" valign="top" background="images/baseBG.png">
+<iframe width="850" height="480" src="https://www.youtube.com/embed/UyM4bMRxzfg?list=PLNgxVZwie3t8GRSZ_-V_CAxgLBzo-A8qV" frameborder="0" allowfullscreen></iframe>
+  <p><span class="style18">***คำแนะนำการใช้งาน*** </span></p>
+  <p><span class="style18">กด <img src="images/Iconplaylist.png" /> เพื่อแสดงรายการทั้งหมด</span></p></td> 
+  </tr>
+</table>
+
+</body>
+</html>
