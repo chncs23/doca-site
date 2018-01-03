93e3dafdd959e612e466b5307a68c59e301a5dec
Choatravee N <ch.n.17699@gmail.com>
2018-01-03 13:43:13 +0700

    แก้ connection และแก้ config การ redirect ของ ckfinder

--- callconnection.php
+++ callconnection.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER","localhost");
-define("DB_USER","root");
-define("DB_PASSWD","");
+define("DB_USER","itdoca");
+define("DB_PASSWD","itDoca@87283");
 define("DB_CURRENT","itdoca_web");
 $db = mysql_connect(DB_SERVER,DB_USER,DB_PASSWD);
 $objDB = mysql_select_db("itdoca_web");
